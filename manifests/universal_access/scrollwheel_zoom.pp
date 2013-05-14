f6b55d3e407d33f1b2499bf0537c422ee6b82511
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-13 23:23:34 -0500

    Rename the Universal Access 'enable_scrollwheel_zoom' to be more customizable and clear

--- manifests/universal_access/scrollwheel_zoom.pp
+++ manifests/universal_access/scrollwheel_zoom.pp
@@ -1,9 +1,15 @@
 # Public: Enable use of mousewheel for zooming.
-class osx::universal_access::enable_scrollwheel_zoom {
+class osx::universal_access::scrollwheel_zoom($enabled = false) {
   boxen::osx_defaults { 'Use mouse wheel (scroll gesture) to zoom':
     user   => $::boxen_user,
     domain => 'com.apple.universalaccess',
     key    => 'closeViewScrollWheelToggle',
-    value  => true;
+    value  => $enabled,
+  }
+}
+
+class osx::universal_access::enable_scrollwheel_zoom {
+  class { 'osx::universal_access::scrollwheel_zoom':
+    enabled => true,
   }
 }
