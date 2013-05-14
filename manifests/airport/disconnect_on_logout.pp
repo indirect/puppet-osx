abab5f8e364171214798ad5433c9476446853a37
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-13 23:31:56 -0500

    Add Airport preference to disconnect on logout

---
+++ manifests/airport/disconnect_on_logout.pp
@@ -0,0 +1,13 @@
+class osx::airport::disconnect_on_logout($enabled) {
+  include osx::airport
+
+  case $enabled {
+    true:  { $enabled_yes_no = 'YES' }
+    false: { $enabled_yes_no = 'NO' }
+  }
+
+  exec { 'Toggle Airport "Disconnect On Logout" Preference':
+    command => "${osx::airport::path_to_binary} prefs DisconnectOnLogout=${enabled_yes_no}",
+    user    => root,
+  }
+}
