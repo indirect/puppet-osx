b005445160d99a8f6f70ffb64d2cd092215e3bec
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-13 23:20:13 -0500

    Rename and refactor the Global 'key_repeat_delay' preference to be more customizable and clear.

--- manifests/keyboard/key_repeat_delay.pp
+++ manifests/keyboard/key_repeat_delay.pp
@@ -5,17 +5,17 @@
 # Examples
 #
 #   # set the repeat delay to 35 (the default)...
-#   include osx::global::key_repeat_delay
+#   include osx::keyboard::key_repeat_delay
 #
 #   # ...or pick your own delay!
-#   class { 'osx::global::key_repeat_delay':
+#   class { 'osx::keyboard::key_repeat_delay':
 #     delay => 10
 #   }
-class osx::global::key_repeat_delay($delay = 35) {
+class osx::keyboard::key_repeat_delay($delay) {
   boxen::osx_defaults { 'key repeat delay':
+    user   => $::boxen_user,
     domain => 'NSGlobalDomain',
     key    => 'InitialKeyRepeat',
     value  => $delay,
-    user   => $::boxen_user
   }
 }
