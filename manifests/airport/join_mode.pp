ccedadb32b6c7122e066f5511624ecfb232f8e78
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-13 23:32:35 -0500

    Add Airport preference to set what to do when joining a preferred network

---
+++ manifests/airport/join_mode.pp
@@ -0,0 +1,10 @@
+class osx::airport::join_mode($mode) {
+  include osx::airport
+
+  # Modes: Automatic | Preferred | Ranked | Recent | Strongest
+
+  exec { 'Set What to Do When Preferred Networks Are Available':
+    command => "${osx::airport::path_to_binary} prefs JoinMode=${mode}",
+    user    => root,
+  }
+}
