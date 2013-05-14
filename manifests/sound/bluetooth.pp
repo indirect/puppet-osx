9ff150f8f6022996ebd791edf5473365c3e139c4
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-14 02:49:40 -0500

    Add a preference which can fix the quality of Bluetooth audio devices

---
+++ manifests/sound/bluetooth.pp
@@ -0,0 +1,10 @@
+class osx::sound::bluetooth(
+  $audio_bitpool) {
+
+  boxen::osx_defaults { 'Set the Audio Bitpool for Bluetooth Audio Devices':
+    user   => $::boxen_user,
+    key    => '"Apple Bitpool Min (editable)"',
+    domain => 'com.apple.BluetoothAudioAgent',
+    value  => $audio_bitpool,
+  }
+}
