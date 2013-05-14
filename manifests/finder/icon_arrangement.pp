caea77c19e3471d4bb55d3f1357763e23b92ea39
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-14 02:12:11 -0500

    Add a preference to change the arrangement of the icons in the Finder and on the Desktop

---
+++ manifests/finder/icon_arrangement.pp
@@ -0,0 +1,8 @@
+class osx::finder::icon_arrangement($style) {
+  exec { [
+    "/usr/libexec/plistbuddy -c 'Set :DesktopViewSettings:IconViewSettings:arrangeBy ${style}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
+    "/usr/libexec/plistbuddy -c 'Set :FK_StandardViewSettings:IconViewSettings:arrangeBy ${style}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
+    "/usr/libexec/plistbuddy -c 'Set :StandardViewSettings:IconViewSettings:arrangeBy ${style}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
+    "/usr/libexec/plistbuddy -c 'Set :ComputerViewSettings:IconViewSettings:arrangeBy ${style}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
+  ]: }
+}
