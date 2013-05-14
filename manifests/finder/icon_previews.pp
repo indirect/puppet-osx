822ad897fbd3a9d55ff4cb0a935dae36cdc1d69b
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-14 02:14:30 -0500

    Add a preference which toggles whether the icon previews are rendered in Finder

---
+++ manifests/finder/icon_previews.pp
@@ -0,0 +1,26 @@
+class osx::finder::icon_previews($enabled) {
+  include osx::finder
+
+  exec { [
+    "/usr/libexec/plistbuddy -c 'Set :FK_StandardViewSettings:IconViewSettings:showIconPreview ${enabled}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
+    "/usr/libexec/plistbuddy -c 'Set :FK_StandardViewSettings:ListViewSettings:showIconPreview ${enabled}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
+    "/usr/libexec/plistbuddy -c 'Set :FK_StandardViewSettings:ExtendedListViewSettings:showIconPreview ${enabled}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
+    "/usr/libexec/plistbuddy -c 'Set :StandardViewSettings:IconViewSettings:showIconPreview ${enabled}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
+    "/usr/libexec/plistbuddy -c 'Set :StandardViewSettings:ListViewSettings:showIconPreview ${enabled}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
+    "/usr/libexec/plistbuddy -c 'Set :StandardViewSettings:ExtendedListViewSettings:showIconPreview ${enabled}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
+    "/usr/libexec/plistbuddy -c 'Set :ComputerViewSettings:IconViewSettings:showIconPreview ${enabled}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
+    "/usr/libexec/plistbuddy -c 'Set :ComputerViewSettings:ListViewSettings:showIconPreview ${enabled}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
+    "/usr/libexec/plistbuddy -c 'Set :ComputerViewSettings:ExtendedListViewSettings:showIconPreview ${enabled}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
+  ]:
+
+    notify => Exec['Remove All .DS_Store Files'],
+  }
+
+  boxen::osx_defaults { 'Remove All Existing Icon Previews':
+    user   => $::boxen_user,
+    key    => 'FXRecentFolders',
+    domain => 'com.apple.finder',
+    value  => (),
+    notify => Exec['killall Finder'],
+  }
+}
