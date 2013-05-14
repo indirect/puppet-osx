a0a63e3b416de48bf58ca6dae7d0ba7e8814f439
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-14 01:50:27 -0500

    Add a preference to set the Dock's icon size

---
+++ manifests/dock/icon_size.pp
@@ -0,0 +1,12 @@
+class osx::dock::icon_size($size) {
+  include osx::dock
+
+  boxen::osx_defaults { 'Sets the Size of the Icons in Pixels':
+    user   => $::boxen_user,
+    key    => 'tilesize',
+    domain => 'com.apple.dock',
+    type   => 'int',
+    value  => $size,
+    notify => Exec['killall Dock'],
+  }
+}
