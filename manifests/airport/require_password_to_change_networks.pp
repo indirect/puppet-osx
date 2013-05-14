87204870ea6f80998937c1f853a65cef61b7206f
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-13 23:59:55 -0500

    Add Airport preference to toggle the requirement of an admin password to change networks

---
+++ manifests/airport/require_password_to_change_networks.pp
@@ -0,0 +1,13 @@
+class osx::airport::require_password_to_change_networks($enabled) {
+  include osx::airport
+
+  case $enabled {
+    true:  { $enabled_yes_no = 'YES' }
+    false: { $enabled_yes_no = 'NO' }
+  }
+
+  exec { 'Toggle Airport "Require Password to Change Networks" Preference':
+    command => "${osx::airport::path_to_binary} prefs RequireAdminNetworkChange=${enabled_yes_no}",
+    user    => root,
+  }
+}
