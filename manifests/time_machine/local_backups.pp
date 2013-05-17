2a0d460fa03791ab13b3a04a6f52b642ad7713a1
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-17 03:01:55 -0500

    Add idempotent check for the local time machine backup preference

--- manifests/time_machine/local_backups.pp
+++ manifests/time_machine/local_backups.pp
@@ -4,8 +4,14 @@ class osx::time_machine::local_backups($enabled) {
     false: { $enabled_text = 'disable' }
   }
 
+  case $enabled {
+    true:  { $enabled_check = 'grep -E  "Kind\s+:\s+Local"' }
+    false: { $enabled_check = 'grep -LE "Kind\s+:\s+Local"' }
+  }
+
   exec { 'Toggle Whether Local Time Machine Backups are Enabled':
-    command => "hash tmutil &> /dev/null && tmutil ${enabled_text}local",
+    command => "tmutil ${enabled_text}local",
+    onlyif  => "hash tmutil &> /dev/null && tmutil destinationinfo | ${enabled_check}",
     user    => root,
   }
 }
