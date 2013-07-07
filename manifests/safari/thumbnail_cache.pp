508db07356730007445d25ffd1e26cfc4623d900
Andre Arko <andre@arko.net>
2013-07-06 20:15:18 -0700

    Safari thumbnail cache had invalid syntax.

--- manifests/safari/thumbnail_cache.pp
+++ manifests/safari/thumbnail_cache.pp
@@ -1,7 +1,7 @@
 class osx::safari::thumbnail_cache($enabled) {
   case $enabled {
-    true:  { $enabled_int: 2 }
-    false: { $enabled_int: 0 }
+    true:  { $enabled_int = 2 }
+    false: { $enabled_int = 0 }
   }
 
   boxen::osx_defaults { 'Toggle Whether the Cache for "Top Sites" and "History" is Enabled':
